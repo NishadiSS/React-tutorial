ba6af2edcee397fd00cc0e415201503526d47b44
NishadiSS <nishadisasanthika260@gmail.com>
2024-05-10 00:49:35 +0530

    created dynamic components

--- React  First Project/my_first_project/src/Components/TopicBox.js
+++ React  First Project/my_first_project/src/Components/TopicBox.js
@@ -2,10 +2,18 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+  const topic = "My channel name is CodeNish"
+
+  //const a=5;
+  //const b=10;
+  //const c=a+b;
+
+  //const foodprices = {rice:"500" , noodles:"350"}
+
   return (
     <div>
       <div className="topicBox">
-        <span className='text'> This is the topic</span>
+        <span className='text'> {topic} </span>
       </div>
     </div>
   )
